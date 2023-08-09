b2750e1c2a0d28c4fe43102bba638266bd75d137
Lebedeva-Elena <lebedeva_leb.en@mail.ru>
2023-08-09 21:45:16 +0300

    сделала дз

---
+++ src/main/java/skypro/employeeBook/controller/EmployeeController.java
@@ -0,0 +1,35 @@
+package skypro.employeeBook.controller;
+
+import org.springframework.web.bind.annotation.*;
+import skypro.employeeBook.dto.Employee;
+import skypro.employeeBook.service.EmployeeService;
+
+import java.util.Collection;
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeController {
+    private EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping("/add")
+    public Employee addEmployee(@RequestParam String firstName, @RequestParam String lastName) {
+        return employeeService.addEmployee(firstName, lastName);
+    }
+    @GetMapping("/remove")
+    public Employee remoteEmployee(@RequestParam String firstName, @RequestParam String lastName) {
+        return employeeService.addEmployee(firstName, lastName);
+    }
+    @GetMapping("/find")
+    public Employee getEmployee(@RequestParam String firstName, @RequestParam String lastName) {
+        return employeeService.addEmployee(firstName, lastName);
+    }
+    @GetMapping
+    public Collection<Employee> findAll(){
+        return employeeService.findAll();
+    }
+
+}
---
+++ src/main/java/skypro/employeeBook/exeption/EmployeeStorageIsFullException.java
@@ -0,0 +1,9 @@
+package skypro.employeeBook.exeption;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "Превышен лимит количества сотрудников в фирме")
+
+public class EmployeeStorageIsFullException extends RuntimeException{
+}
